955a46182c0dd50120367a37de575b287de0cd4a
Agustin <zagoagus@gmail.com>
2024-12-17 13:10:26 -0300

    Refactor layout and hero components; update navbar and link styles; add primary color to Tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,14 +3,24 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    colors: {
+      'primary': '#F8B90E',
+      'secondary': '#970F00',
+      'background': '#F0E6CE',
+      'title': '#474141',
+      'babaganoush': '#F0E6CE',
+      'ajiamarillo': '#FFBB00',
+      'muhamara': '#970F00',
+      'mayonesa': '#F6DA9D',
+},
     extend: {
       fontFamily: {
         sans: [
-          "Bricolage Grotesque Variable",
+          "Montserrat",
           "Inter Variable",
           "Inter",
-          ...defaultTheme.fontFamily.sans,
-        ],
+          ...defaultTheme.fontFamily.sans,],
+
       },
     },
   },
